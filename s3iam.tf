756b6aa28331e30394c629cf8ed66d9621b91766
david-wirelab <info@wirelab.co.uk>
2017-11-29 20:29:50 +0000

    typo fix

--- s3iam.tf
+++ s3iam.tf
@@ -1,31 +1,20 @@
-/*********************************
-* Setup S3 policy
-**********************************/
+
 resource "aws_iam_policy" "HAProxyBucketPolicy" {
   name        = "HAProxyBucketPolicy"
   policy      = "${file("s3policy.json")}"
 }
 
-/*********************************
-* Setup EC2 role for S3
-**********************************/
 resource "aws_iam_role" "HAProxyECServer2Role" {
   name               = "HAProxyECServer2Role"
-  assume_role_policy = "${file("s3assume.json")}"
+  assume_role_policy = "${file("s3assumerole.json")}"
 }
 
-/*********************************
-* Attach policy to role
-**********************************/
 resource "aws_iam_policy_attachment" "attachS3_bucket_policy" {
   name       = "attachS3_bucket_policy"
   roles      = ["${aws_iam_role.HAProxyECServer2Role.name}"]
   policy_arn = "${aws_iam_policy.HAProxyBucketPolicy.arn}"
 }
 
-/*********************************
-* Create instance profile for EC2
-**********************************/
 resource "aws_iam_instance_profile" "HAProxyServerInstanceProfile" {
   name  = "HAProxyServerInstanceProfile"
   role = "${aws_iam_role.HAProxyECServer2Role.name}"
