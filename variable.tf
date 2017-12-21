267b6131cdfac3721f8e4499705eda2229ea1923
david-wirelab <info@wirelab.co.uk>
2017-12-21 15:57:14 +0000

    add S3 bucket and move variables over

--- variable.tf
+++ variable.tf
@@ -2,6 +2,24 @@ variable "peeringvpc_id" {}
 variable "haproxy_private_ip" {}
 variable "haproxy_subnet_cidr_block" {}
 
+variable "route_table_id" {}
+
+variable "region" {
+  default = "eu-west-2"
+}
+
+variable "s3_bucket_name" {
+  type = "map"
+}
+
+variable "s3_bucket_acl" {
+  type = "map"
+}
+
+variable "s3_bucket_visibility" {
+  default = "private"
+}
+
 variable "name_prefix" {
   default = "dq-peering-"
 }
