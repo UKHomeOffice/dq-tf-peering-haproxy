cbedbc4fa5e95ee98f881b0b6e4f50d879145649
david-wirelab <info@wirelab.co.uk>
2019-05-14 14:35:59 +0100

    Remove test FMS proxy EC2 instance

--- instance.tf
+++ instance.tf
@@ -44,29 +44,6 @@ resource "aws_instance" "peeringhaproxy2" {
   }
 }
 
-resource "aws_instance" "peeringhaproxy3" {
-  ami                    = "${data.aws_ami.dq-peering-haproxy.id}"
-  instance_type          = "${var.instance_type}"
-  subnet_id              = "${aws_subnet.haproxy_subnet.id}"
-  vpc_security_group_ids = ["${aws_security_group.haproxy.id}"]
-  private_ip             = "${var.haproxy_private_ip3}"
-  user_data              = "${var.s3_bucket_name}"
-  key_name               = "${var.key_name}"
-  iam_instance_profile   = "${aws_iam_instance_profile.haproxy_server_instance_profile.id}"
-
-  lifecycle {
-    prevent_destroy = true
-
-    ignore_changes = [
-      "ami",
-    ]
-  }
-
-  tags = {
-    Name = "ec2-fms-test-${local.naming_suffix}"
-  }
-}
-
 resource "aws_security_group" "haproxy" {
   vpc_id = "${var.peeringvpc_id}"
 
