cbbe550c14f9777760939a8a45aeb8d2d9b8e42c
Srikanth Arunachalam <116078089+s-arunachalam@users.noreply.github.com>
2023-09-15 05:13:32 +0100

    fix: yel-8016 Modified to enforce imdsv2

--- instance.tf
+++ instance.tf
@@ -28,6 +28,9 @@ set -e
 #log output from this user_data script
 exec > >(tee /var/log/user-data.log|logger -t user-data ) 2>&1
 
+echo "Enforcing imdsv2 on ec2 instance"
+curl http://169.254.169.254/latest/meta-data/instance-id | xargs -I {} aws ec2 modify-instance-metadata-options --instance-id {} --http-endpoint enabled --http-tokens required
+
 echo "#Create env_vars file"
 touch /home/ec2-user/env_vars
 echo "export s3_bucket_name=s3-dq-peering-haproxy-config-bucket-${var.namespace}" > /home/ec2-user/env_vars
